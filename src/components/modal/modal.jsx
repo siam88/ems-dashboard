633c8060d32dca86e75e465b104b91f86c1a94a6
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2021-02-05 15:31:40 +0600

    content field added

---
+++ src/components/modal/modal.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
+
+const CustomModal = ({ props }) => {
+    
+
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default CustomModal
--- src/containers/Content/Content.jsx
+++ src/containers/Content/Content.jsx
@@ -1,87 +1,46 @@
-import React from 'react'
-import { Table, Tag, Space } from 'antd';
+import React, { useState } from 'react';
+import { Table, Tag, Space, Typography, Input} from 'antd';
+import SliderTable from '../../components/Table/SliderTable/SliderTable'
+import CustomModal from '../../components/modal/modal';
+import { UserOutlined } from '@ant-design/icons';
+import { Modal, Button } from 'antd';
 
+const { Title } = Typography;
+const Content = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
 
-const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      render: text => <a>{text}</a>,
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: 'Tags',
-      key: 'tags',
-      dataIndex: 'tags',
-      render: tags => (
-        <>
-          {tags.map(tag => {
-            let color = tag.length > 5 ? 'geekblue' : 'green';
-            if (tag === 'loser') {
-              color = 'volcano';
-            }
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-          })}
-        </>
-      ),
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (text, record) => (
-        <Space size="middle">
-          <a>Invite {record.name}</a>
-          <a>Delete</a>
-        </Space>
-      ),
-    },
-  ];
-  
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      tags: ['loser'],
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
-    },
-  ];
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
 
-const Content=()=>{
-    return(
-        <div>
-            <Table columns={columns} dataSource={data} />
-        </div>
-    )
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  return (
+    <div>
+      <Title>Home Silider info Table</Title>
+
+      <Button type="primary" onClick={showModal}>
+        Add Slider
+      </Button>
+      <Modal title="Slider Information adding field" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <Input size="large" placeholder="Image Title" prefix={<UserOutlined />} />
+    <br />
+    <br />
+        <Input size="large" placeholder="Image Description" prefix={<UserOutlined />} />
+    <br />
+    <br />
+        <Input size="large" placeholder="Image Source" prefix={<UserOutlined />} />
+ 
+
+        </Modal>
+      <SliderTable />
+    </div>
+  )
 }
 
 export default Content
